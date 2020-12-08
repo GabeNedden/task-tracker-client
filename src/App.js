a0a30f8d11cbc5079e6528e1af87f0ee1c8abeac
GabeNedden <gabenedden@gmail.com>
2020-12-07 21:58:05 -0500

    fix context

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-import { AuthContext, AuthProvider } from './context/auth';
+import { AuthProvider } from './context/auth';
 
 import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,7 +3,7 @@ import { Button, Form } from 'semantic-ui-react';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
-import { AuthContext, AuthProvider } from '../context/auth';
+import { AuthContext } from '../context/auth';
 import { useForm } from '../utilities/hooks';
 
 
