3c1c780cbeac11ce72bf684e61af94313a86b4b4
GabeNedden <gabenedden@gmail.com>
2021-01-25 21:48:35 -0500

    add button to close teamCard

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Button, Card, Feed } from 'semantic-ui-react';
+import { Button, Card, Feed, Icon } from 'semantic-ui-react';
 
 import { AuthContext } from '../context/auth';
 
@@ -7,36 +7,46 @@ import { AuthContext } from '../context/auth';
 function TeamCard({ teammembers }) {
 
   const { user } = useContext(AuthContext);
+  const [teamView, setTeamView] = React.useState(true)
+  const hideTeam = () => setTeamView(false)
+
+  const Team = () => (
+    <Card>
+        <Card.Content>
+            <Icon onClick={hideTeam} style={{float:"right", cursor:'pointer'}} name='close' />
+            <Card.Header>Team Members</Card.Header>
+        </Card.Content>
+        <Card.Content>
+            <Feed>
+                {teammembers.map(teammember => (
+                    <Feed.Event>
+                        <Feed.Label>
+                            <img alt={user} src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+                        </Feed.Label>
+                        <Feed.Content>
+                            {teammember.username}
+                        </Feed.Content>
+                        <Feed.Content>
+                        <Button circular
+                            icon="terminal"
+                            as="div"
+                            color="orange"
+                            floated="right"
+                        />
+                        </Feed.Content>
+                    </Feed.Event>
+                ))}
+            </Feed>
+        </Card.Content>
+    </Card>
+  )
 
     return (
-        <Card>
-            <Card.Content>
-                <Card.Header>Team Members</Card.Header>
-            </Card.Content>
-            <Card.Content>
-                <Feed>
-                    {teammembers.map(teammember => (
-                        <Feed.Event>
-                            <Feed.Label>
-                                <img alt={user} src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
-                            </Feed.Label>
-                            <Feed.Content>
-                                {teammember.username}
-                            </Feed.Content>
-                            <Feed.Content>
-                            <Button circular
-                                icon="terminal"
-                                as="div"
-                                color="orange"
-                                floated="right"
-                            />
-                            </Feed.Content>
-                        </Feed.Event>
-                    ))}
-                </Feed>
-            </Card.Content>
-        </Card>
+        <div>
+            { teamView ? <Team /> : null }
+        </div>
     )
+
 }
   
   export default TeamCard;
