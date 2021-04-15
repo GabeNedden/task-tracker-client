3d872844c63f9657f8c68160b98ba1a22ff45a00
GabeNedden <gabenedden@gmail.com>
2021-04-14 21:56:02 -0400

    Test(Navbar): add responsive navbar container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react'
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
@@ -8,26 +7,24 @@ import './App.css';
 import { AuthProvider } from './context/auth';
 import AuthRoute from './utilities/AuthRoute';
 
-
-import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Matrix from './pages/Matrix';
 import ProjectPage from './pages/ProjectPage';
+import ResponsiveContainer from './components/ResponsiveContainer';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-      <Container>
-        <MenuBar />
-        <Route exact path='/' component={Home}/>
-        <AuthRoute exact path='/login' component={Login}/>
-        <AuthRoute exact path='/register' component={Register}/>
-        <AuthRoute exact path='/matrix' component={Matrix}/>
-        <Route exact path='/project/:projectId' component={ProjectPage}/>
-      </Container>
+        <ResponsiveContainer>
+          <Route exact path='/' component={Home}/>
+          <AuthRoute exact path='/login' component={Login}/>
+          <AuthRoute exact path='/register' component={Register}/>
+          <AuthRoute exact path='/matrix' component={Matrix}/>
+          <Route exact path='/project/:projectId' component={ProjectPage}/>
+        </ResponsiveContainer>
     </Router>
     </AuthProvider>
   );
